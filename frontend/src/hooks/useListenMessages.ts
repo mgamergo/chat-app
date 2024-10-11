042e824678f8bd0757e32ac3ff37ce1da600acb3
mgamergo <chaitanyaswaroop36@gmail.com>
2024-10-11 14:23:04 +0530

    Added notification sound for incoming messages

--- frontend/src/hooks/useListenMessages.ts
+++ frontend/src/hooks/useListenMessages.ts
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversations";
+import notification from '../assets/sounds/notification.mp3'
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
@@ -8,6 +9,8 @@ const useListenMessages = () => {
 
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
+        const sound = new Audio(notification)
+        sound.play()
         setMessages([...messages, newMessage])
     })
 
